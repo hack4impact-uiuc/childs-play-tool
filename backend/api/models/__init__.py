de98e9a2925171a35c4c79b5d2e93777e76c8d8a
Arpan Laha <arpanlaha99@gmail.com>
2018-10-08 23:53:38 -0500

    added game and ranking

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,6 +1,9 @@
 # this file structure follows http://flask.pocoo.org/docs/1.0/patterns/appfactories/
 # initializing db in api.models.base instead of in api.__init__.py
 # to prevent circular dependencies
-from .Email import Email
-from .Person import Person
+#from .Email import Email
+#from .Person import Person
+from .Game import Game
+from .Ranking import Ranking
+#from .rankings import Ranking
 from .base import db
