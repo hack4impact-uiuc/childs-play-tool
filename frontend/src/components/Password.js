cb53c51146b3cbee090fe866a87aedac25581115
Albert Cao <albertc4@illinois.edu>
2018-10-08 23:47:34 -0500

    reformatted

--- frontend/src/components/Password.js
+++ frontend/src/components/Password.js
@@ -2,22 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-
-
 class Password extends Component {
   render() {
     return (
       <div className="Password">
         <form>
-            <label>
-                Key:
-                <input />
-            </label>
-            <button type="button">Submit</button>
+          <label>
+            Key:
+            <input />
+          </label>
+          <button type="button">Submit</button>
         </form>
       </div>
     )
   }
 }
 
-export default (Password)
+export default Password
