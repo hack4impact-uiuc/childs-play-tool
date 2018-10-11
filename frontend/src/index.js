8c67176899d0ea5793e03dacbb27c8e904bc6984
Albert Cao <albertc4@illinois.edu>
2018-10-11 18:22:01 -0500

    reformatted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,9 @@ import { Provider } from 'react-redux'
 import { Route } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
 // import './styles/index.css'
-import { Counter } from './components'
-import { Password } from './components'
-import { SearchPage, Dropdown } from './components'
+import { Counter, Password, SearchPage, DropDown, Update } from './components'
 import registerServiceWorker from './registerServiceWorker'
 import configureStore, { history } from './redux/configureStore'
-import { Update } from './components'
 
 const store = configureStore()
 
