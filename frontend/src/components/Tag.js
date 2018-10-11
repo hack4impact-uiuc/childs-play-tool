0600493ecca6f0cc176cbec3d078915d74ae0563
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2018-10-11 18:04:59 -0500

    Cleaned up tag code

--- frontend/src/components/Tag.js
+++ frontend/src/components/Tag.js
@@ -1,47 +1,29 @@
 import React, { Component } from 'react'
-
-const ageTag = {
-  background: '#f998e8',
-  color: 'white',
-  width: '55px'
-}
-const ailmentTag = {
-  background: '#a2f998',
-  color: 'white',
-  width: '55px'
-}
-const systemTag = {
-  background: '#98d1f9',
-  color: 'white',
-  width: '55px'
+const style = {
+  age: {
+    background: '#f998e8',
+    color: 'white',
+    width: '55px'
+  },
+  ailment: {
+    background: '#a2f998',
+    color: 'white',
+    width: '55px'
+  },
+  system: {
+    background: '#98d1f9',
+    color: 'white',
+    width: '55px'
+  }
 }
 
 class Tag extends Component {
-  drawTags = (type, tag) => {
-    if (type === 'age') {
-      return (
-        <div align="center" style={ageTag}>
-          {tag}
-        </div>
-      )
-    } else if (type === 'ailment') {
-      return (
-        <div align="center" style={ailmentTag}>
-          {tag}
-        </div>
-      )
-    } else if (type === 'system') {
-      return (
-        <div align="center" style={systemTag}>
-          {tag}
-        </div>
-      )
-    } else {
-      return <div>{tag}</div>
-    }
-  }
   render() {
-    return <div className="Tag">{this.drawTags(this.props.type, this.props.tag)}</div>
+    return (
+      <div className="Tag" align="center" style={style[this.props.type]}>
+        {this.props.tag}
+      </div>
+    )
   }
 }
 
