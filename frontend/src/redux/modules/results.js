f080dc5fa061994975320a6aec7f433ba5c2afd6
Michael Chen <36244017+Feeshay@users.noreply.github.com>
2018-10-16 20:46:23 -0500

    Results Page by Console (#29)
    * basic tabs added
    
    * Results component and redux now divide by system
    
    * dont roast me

--- frontend/src/redux/modules/results.js
+++ frontend/src/redux/modules/results.js
@@ -1,79 +1,85 @@
 // @flow
 
 const initialState = {
-  games: [
-    {
-      title: 'Mario Kart',
-      summary: 'A racing game',
-      description: 'A racing game where the most important skill is luck',
-      tags: [
-        { type: 'age', tag: '0-5' },
-        { type: 'system', tag: 'Switch' },
-        { type: 'ailment', tag: 'Pain' }
-      ]
-    },
-    {
-      title: 'Childs Play',
-      summary: 'Best team',
-      description: 'Best team to ever exist ever',
-      tags: [
-        { type: 'age', tag: 'Bounce' },
-        { type: 'system', tag: 'that' },
-        { type: 'ailment', tag: 'shit' }
-      ]
-    },
-    {
-      title: 'Test',
-      summary: 'A test',
-      description: 'A test',
-      tags: [
-        { type: 'age', tag: '3' },
-        { type: 'system', tag: 'xbox' },
-        { type: 'ailment', tag: 'debug' },
-        { type: 'misc', tag: 'test' }
-      ]
-    },
-    {
-      title: 'Test',
-      summary: 'A test',
-      description: 'A test',
-      tags: [
-        { type: 'age', tag: '3' },
-        { type: 'system', tag: 'xbox' },
-        { type: 'ailment', tag: 'debug' }
-      ]
-    },
-    {
-      title: 'Test',
-      summary: 'A test',
-      description: 'A test',
-      tags: [
-        { type: 'age', tag: '3' },
-        { type: 'system', tag: 'xbox' },
-        { type: 'ailment', tag: 'debug' }
-      ]
-    },
-    {
-      title: 'Test',
-      summary: 'A test',
-      description: 'A test',
-      tags: [
-        { type: 'age', tag: '3' },
-        { type: 'system', tag: 'xbox' },
-        { type: 'ailment', tag: 'debug' }
-      ]
-    },
-    {
-      title: 'Test',
-      summary: 'A test',
-      description: 'A test',
-      tags: [
-        { type: 'age', tag: '3' },
-        { type: 'system', tag: 'xbox' },
-        { type: 'ailment', tag: 'debug' }
-      ]
-    }
-  ]
+  games: {
+    Switch: [
+      {
+        title: 'Mario Kart',
+        summary: 'A racing game',
+        description: 'A racing game where the most important skill is luck',
+        tags: [
+          { type: 'age', tag: '0-5' },
+          { type: 'system', tag: 'Switch' },
+          { type: 'symptom', tag: 'Pain' }
+        ]
+      }
+    ],
+    That: [
+      {
+        title: 'Childs Play',
+        summary: 'Best team',
+        description: 'Best team to ever exist ever',
+        tags: [
+          { type: 'age', tag: 'Bounce' },
+          { type: 'system', tag: 'that' },
+          { type: 'symptom', tag: 'shit' }
+        ]
+      }
+    ],
+    XBox: [
+      {
+        title: 'Test',
+        summary: 'A test',
+        description: 'A test',
+        tags: [
+          { type: 'age', tag: '3' },
+          { type: 'system', tag: 'xbox' },
+          { type: 'symptom', tag: 'debug' },
+          { type: 'misc', tag: 'test' }
+        ]
+      },
+      {
+        title: 'Test',
+        summary: 'A test',
+        description: 'A test',
+        tags: [
+          { type: 'age', tag: '3' },
+          { type: 'system', tag: 'xbox' },
+          { type: 'symptom', tag: 'debug' }
+        ]
+      },
+      {
+        title: 'Test',
+        summary: 'A test',
+        description: 'A test',
+        tags: [
+          { type: 'age', tag: '3' },
+          { type: 'system', tag: 'xbox' },
+          { type: 'symptom', tag: 'debug' }
+        ]
+      },
+      {
+        title: 'Test',
+        summary: 'A test',
+        description: 'A test',
+        tags: [
+          { type: 'age', tag: '3' },
+          { type: 'system', tag: 'xbox' },
+          { type: 'symptom', tag: 'debug' }
+        ]
+      },
+      {
+        title: 'Test',
+        summary: 'A test',
+        description: 'A test',
+        tags: [
+          { type: 'age', tag: '3' },
+          { type: 'system', tag: 'xbox' },
+          { type: 'symptom', tag: 'debug' }
+        ]
+      }
+    ]
+  }
 }
 
 export default function reducer(state = initialState, action) {
