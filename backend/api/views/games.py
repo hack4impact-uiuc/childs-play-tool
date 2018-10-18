863cf58119713510176d5845bebdd5f77fd71d3d
Arpan Laha <arpanlaha99@gmail.com>
2018-10-18 15:11:08 -0500

    fixed ranking functionality

--- backend/api/views/games.py
+++ backend/api/views/games.py
@@ -1,6 +1,5 @@
 from api.models import db, Game, Ranking
 from api.core import create_response, serialize_list, Mixin, logger
-#from api.auth_tokens import token_required
 from flask import Blueprint, request, jsonify
 import json
 from collections import defaultdict
@@ -41,31 +40,16 @@ def get_games():
                 message="This system has no appropriate games for the specified age and symptom",
             )
         
-        #rankings = db.session.query(Ranking.rank, Ranking.game_id).order_by(Ranking.rank).filter(Ranking.system == system, Ranking.age == age, Ranking.symptom == symptom).subquery()
-        #rankings = db.sessionn.query(Ranking.rank, Ranking.game_id).filter(Ranking.system == system, Ranking.age == age, Ranking.symptom == symptom).order_by(Ranking.rank)
-        # rankings = rankings.subquery()
-        # games = db.session.query(Game.name, Game.gender).filter(Game.system == system).subquery()
-        # ranked_games = games.join(rankings)
-        # logger.info(type(ranked_games))
-        # logger.info(type(rankings))
-        q = db.session.query(Game, Ranking).join(Ranking).filter(Ranking.system == system).all()
-        print(q)
-        #ranked_games = ranked_games.order_by(rank);
-        '''
-        for g in games:
-            r = Ranking.query.filter(Ranking.game_id == g.id)
-            ranked_games.append(r[0])
-            ranked_games.sort(key=lambda x: x.rank, reverse=True)
-            '''          
-    return create_response(status=200, data={"games": serialize_list(q)})
+        ranked_games = db.session.query(Game.name, Game.gender, Game.system, Ranking.rank).join(Ranking).filter(Ranking.system == system, Ranking.symptom == symptom, Ranking.age == age).order_by(Ranking.rank).all()
+        ranked_games = [dict(zip(ranked_game.keys(), ranked_game)) for ranked_game in ranked_games]
+
+    return create_response(status=200, data={"games": ranked_games})
 
 
 @games_page.route(GAMES_ID_URL, methods=["GET"])
 def get_game_specific(game_id):
-    #game = Game.query.filter()
     game = Game.query.filter(Game.id == game_id)
     if game.count() == 0:
         return create_response(status=400, message="Game not found")
     else:
-        #return create_response(data={"game": serialize_list(game)})
         return create_response(data={"game": serialize_list(game)})
