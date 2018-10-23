9c38f1230f4270806a50dc87c2604644b5e52f5f
Arpan Laha <arpanlaha99@gmail.com>
2018-10-22 23:53:22 -0500

    post request written, need to fix indexing

--- backend/api/models/Game.py
+++ backend/api/models/Game.py
@@ -12,17 +12,18 @@ class Game(Mixin, db.Model):
     name = db.Column(db.String, nullable=False)
     system = db.Column(systems, nullable=False)
     gender = db.Column(genders, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    thumbnail = db.Column(db.LargeBinary, nullable=False)
-    image = db.Column(db.LargeBinary, nullable=False)
+    #description = db.Column(db.String, nullable=False)
+    #thumbnail = db.Column(db.LargeBinary, nullable=False)
+    #image = db.Column(db.LargeBinary, nullable=False)
 
     def __init__(self, data):
         self.id = data["id"]
         self.name = data["name"]
         self.system = data["system"]
         self.gender = data["gender"]
-        self.thumbnail = data["thumbnail"]
-        self.image = data["image"]
+        #self.description = data["description"]
+        #self.thumbnail = data["thumbnail"]
+        #self.image = data["image"]
 
     def __repr__(self):
         return "<name {}>".format(self.name)
