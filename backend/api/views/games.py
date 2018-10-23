7f8578cee1856e14f96aa9ab1c5d34511faffd7b
Arpan Laha <32147742+arpanlaha@users.noreply.github.com>
2018-10-22 23:54:54 -0500

    post request written, need to fix indexing

--- backend/api/views/games.py
+++ backend/api/views/games.py
@@ -82,6 +82,7 @@ def get_games():
 @games_page.route(GAMES_ID_URL, methods=["GET"])
 def get_game_specific(game_id):
     game = Game.query.filter(Game.id == game_id)
+    print(game.first())
     if game.count() == 0:
         return create_response(status=400, message="Game not found")
     else:
@@ -94,11 +95,12 @@ def post_games():
     db.drop_all()
     db.create_all()
     file = request.files["file"]
-    book = xlrd.open_workbook(file)
+    book = xlrd.open_workbook(file_contents=file.read())
     NUMBER_RANKING = 25
     NUMBER_CATEGORIES = 12
     BEGIN_SPACE = 3
     CATEGORY_SPACE = 28
+    i = 0
     for sheet in book.sheets():
         system = sheet.cell(0,1).value
         for symptom_index in range(NUMBER_CATEGORIES):
@@ -106,44 +108,70 @@ def post_games():
                 age_index = symptom_index % 2
                 same_game = Game.query.filter(Game.name == sheet.cell(row, 2 * age_index + 1).value, Game.system == system)
                 if (same_game.count() == 0):
-                    # extra_data = get_giantbomb_data(sheet.cell(row, 2 * age_index + 1), system)
-                    game = {}
-                    game["system"] = system
-                    game["name"] = sheet.cell(row, 2 * age_index + 1).value
-                    game["gender"] = sheet.cell(row, 2 * age_index + 2).value
-                    # game["thumbnail"] = extra_data["thumbnail"]
-                    # game["image"] = extra_data["image"]
-                    # game["description"] = extra_data["description"]
-                    g = Game(game)
-                    db.session.add(g)
-            db.commit()
+                    #extra_data = get_giantbomb_data(sheet.cell(row, 2 * age_index + 1), system)
+                    #print(i)
+                    if len(sheet.cell(row, 2 * age_index + 1).value) != 0:
+                        game = {}
+                        game["system"] = system
+                        game["name"] = sheet.cell(row, 2 * age_index + 1).value
+                        game["gender"] = sheet.cell(row, 2 * age_index + 2).value
+                        game["id"] = i
+                        i = i + 1
+                        #game["thumbnail"] = extra_data["thumbnail"]
+                        #game["image"] = extra_data["image"]
+                        #game["description"] = extra_data["description"]
+                        g = Game(game)
+                        db.session.add(g)
+                    #print(g)
+            db.session.commit()
+            
     '''
     add games
     '''
-    for sheet in book.sheets:
+    i = 0
+    for sheet in book.sheets():
         system = sheet.cell(0,1).value
+        
         for symptom_index in range(6):
             for age_index in range(2):
+                #print("system index")
                 system_symptom_age = sheet.cell(2 + 28 * symptom_index, 1 + 2 * age_index).value
-                descriptors = system_symptom_age.split(" - ")
-                symptom = descriptors[1]
-                age = descriptors[2]
+                descriptors = system_symptom_age.split("-")
+                symptom = descriptors[1].strip()
+                if symptom == "Pain Management":
+                    symptom = "Pain"
+                elif symptom == "Calming":
+                    symptom = "Anxiety/Hyperactivity"
+                elif symptom == "Cheering":
+                    symptom = "Sadness"
+                elif symptom == "Fuzzy":
+                    symptom = "Cognitive Impairment"    
+                age = descriptors[2].strip()
+                if age == "13 and Above":
+                    age = "13 and Older"
                 for game_index in range(25):
-                    rank = int(sheet.cell(3 + 28 * symptom_index, 0).value)
-                    name = sheet.cell(3 + 28 * symptom_index, 1).value
-                    game_id = Game.query(Game.id).filter(Game.name == name).first().id
-                    ranking_id = i
-                    i = i + 1
-                    ranking = {}
-                    ranking["id"] = ranking_id
-                    ranking["age"] = age
-                    ranking["system"] = system
-                    ranking["symptom"] = symptom
-                    ranking["game_id"] = game_id
-                    ranking["rank"] = rank
-                    r = Ranking(ranking)
-                    db.session.add(r)
-    db.commit()
+                    #print(game_index)
+                    #print(3 + 28 * symptom_index + game_index)
+                    #print(sheet.cell(3 + 28 * symptom_index + game_index, 1 + 2 * age_index).value)
+                    #print("rank index")
+                    rank = int(sheet.cell(3 + 28 * symptom_index + game_index, 0).value)
+                    #print("name index")
+                    name = sheet.cell(3 + 28 * symptom_index + game_index, 1 + 2 * age_index).value
+                    if (len(name) != 0):
+                        game_id = Game.query.filter(Game.name == name).first().id                    
+                        #print("put id")
+                        ranking_id = i
+                        i = i + 1
+                        ranking = {}
+                        ranking["id"] = ranking_id
+                        ranking["age"] = age
+                        ranking["system"] = system
+                        ranking["symptom"] = symptom
+                        ranking["game_id"] = game_id
+                        ranking["rank"] = rank
+                        r = Ranking(ranking)
+                        db.session.add(r)
+    db.session.commit()
     
     '''
     for each grouping of 25 (unique):
