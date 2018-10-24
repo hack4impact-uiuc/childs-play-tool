15d1734c647a422888209a1026f8041c8328e1af
Albert Cao <albertc4@illinois.edu>
2018-10-24 17:53:21 -0500

    update

--- backend/scrape/game_scrape.py
+++ backend/scrape/game_scrape.py
@@ -7,71 +7,71 @@ import math
 #do stuff
 #internal counter that will stop
 
-book = xlrd.open_workbook("Sept2018.xlsx")
-i = 0
-for sheet in book.sheets():
-    system = sheet.cell(0,1).value
-    count = 0
-    current_row = 0
-    while (count != 12):
-        while (sheet.cell(current_row, 0).value != 1):
-            current_row += 1
-        initial_row = current_row
-        for age_index in range(2):
-            name = sheet.cell(row, 2 * age_index + 1).value
-            while (name != ""):
-                same_game = Game.query.filter(Game.name == name, Game.system == system)
-                if (same_game.count() == 0):
-                    # extra_data = get_giantbomb_data(name, system)
-                    game = {}
-                    game["system"] = system
-                    game["name"] = name
-                    game["gender"] = sheet.cell(current_row, 2 * age_index + 2).value
-                    game["id"] = i
-                    i = i + 1
-                    # game["thumbnail"] = extra_data["thumbnail"]
-                    # game["image"] = extra_data["image"]
-                    # game["description"] = extra_data["description"]
-                    g = Game(game)
-                    db.session.add(g)
-                current_row += 1
-                if (current_row == sheet.nrows):
-                    break
-                name = sheet.cell(current_row, 2 * age_index + 1).value
-            if (sheet.ncols < 5):
-                count += 2
-                break
-            if (age_index == 0):
-                current_row = initial_row
-            count += 1
+# book = xlrd.open_workbook("Sept2018.xlsx")
+# i = 0
+# for sheet in book.sheets():
+#     system = sheet.cell(0,1).value
+#     count = 0
+#     current_row = 0
+#     while (count != 12):
+#         while (sheet.cell(current_row, 0).value != 1):
+#             current_row += 1
+#         initial_row = current_row
+#         for age_index in range(2):
+#             name = sheet.cell(current_row, 2 * age_index + 1).value
+#             while (name != ""):
+#                 same_game = Game.query.filter(Game.name == name, Game.system == system)
+#                 if (same_game.count() == 0):
+#                     # extra_data = get_giantbomb_data(name, system)
+#                     game = {}
+#                     game["system"] = system
+#                     game["name"] = name
+#                     game["gender"] = sheet.cell(current_row, 2 * age_index + 2).value
+#                     game["id"] = i
+#                     i = i + 1
+#                     # game["thumbnail"] = extra_data["thumbnail"]
+#                     # game["image"] = extra_data["image"]
+#                     # game["description"] = extra_data["description"]
+#                     g = Game(game)
+#                     db.session.add(g)
+#                 current_row += 1
+#                 if (current_row == sheet.nrows):
+#                     break
+#                 name = sheet.cell(current_row, 2 * age_index + 1).value
+#             if (sheet.ncols < 5):
+#                 count += 2
+#                 break
+#             if (age_index == 0):
+#                 current_row = initial_row
+#             count += 1
 
-book = xlrd.open_workbook("Sept2018.xlsx")
-i = 0
-for sheet in book.sheets():
-    system = sheet.cell(0,1).value
-    count = 0
-    current_row = 0
-    print(system)
-    while (count != 12):
-        while (sheet.cell(current_row, 0).value != 1):
-            current_row += 1
-        initial_row = current_row
-        for age_index in range(2):
-            name = sheet.cell(current_row, 2 * age_index + 1).value
-            while (name != ""):
-                print(name)
-                current_row += 1
-                if (current_row == sheet.nrows):
-                    break
-                name = sheet.cell(current_row, 2 * age_index + 1).value
-            if (sheet.ncols < 5):
-                count += 2
-                print("\n")
-                break
-            if (age_index == 0):
-                current_row = initial_row
-            count += 1
-            print("\n")
+# book = xlrd.open_workbook("Sept2018.xlsx")
+# i = 0
+# for sheet in book.sheets():
+#     system = sheet.cell(0,1).value
+#     count = 0
+#     current_row = 0
+#     print(system)
+#     while (count != 12):
+#         while (sheet.cell(current_row, 0).value != 1):
+#             current_row += 1
+#         initial_row = current_row
+#         for age_index in range(2):
+#             name = sheet.cell(current_row, 2 * age_index + 1).value
+#             while (name != ""):
+#                 print(name)
+#                 current_row += 1
+#                 if (current_row == sheet.nrows):
+#                     break
+#                 name = sheet.cell(current_row, 2 * age_index + 1).value
+#             if (sheet.ncols < 5):
+#                 count += 2
+#                 print("\n")
+#                 break
+#             if (age_index == 0):
+#                 current_row = initial_row
+#             count += 1
+#             print("\n")
             
 
 # NUMBER_RANKING = 25
