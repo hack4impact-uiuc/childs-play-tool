2af546a48df3ac01b166b405b4dfe7d158b94de6
Albert Cao <albertc4@illinois.edu>
2018-10-24 17:06:59 -0500

    created more reslient scrape in game_scrape.py

--- backend/scrape/game_scrape.py
+++ backend/scrape/game_scrape.py
@@ -1,6 +1,6 @@
 import xlrd
 import math
-from ../api/models import db, Game, Ranking
+# from ../api/models import db, Game, Ranking
 
 
 #skip white space until ranking
@@ -13,11 +13,11 @@ for sheet in book.sheets():
     system = sheet.cell(0,1).value
     count = 0
     current_row = 0
-    while (count != 6):
-        while (sheet.cell(current_row, 0).value != "1"):
+    while (count != 12):
+        while (sheet.cell(current_row, 0).value != 1):
             current_row += 1
         initial_row = current_row
-        for age_block in range(2):
+        for age_index in range(2):
             name = sheet.cell(row, 2 * age_index + 1).value
             while (name != ""):
                 same_game = Game.query.filter(Game.name == name, Game.system == system)
@@ -35,33 +35,67 @@ for sheet in book.sheets():
                     g = Game(game)
                     db.session.add(g)
                 current_row += 1
-                name = sheet.cell(row, 2 * age_index + 1).value
-            if (age_block == 0):
+                if (current_row == sheet.nrows):
+                    break
+                name = sheet.cell(current_row, 2 * age_index + 1).value
+            if (sheet.ncols < 5):
+                count += 2
+                break
+            if (age_index == 0):
                 current_row = initial_row
             count += 1
 
-NUMBER_RANKING = 25
-NUMBER_CATEGORIES = 12
-BEGIN_SPACE = 3
-CATEGORY_SPACE = 28
 book = xlrd.open_workbook("Sept2018.xlsx")
+i = 0
 for sheet in book.sheets():
     system = sheet.cell(0,1).value
-    for symptom_index in range(NUMBER_CATEGORIES):
-        for row in range(BEGIN_SPACE + CATEGORY_SPACE * math.floor(symptom_index/2), BEGIN_SPACE + CATEGORY_SPACE * math.floor(symptom_index/2) + NUMBER_RANKING, 1):
-            age_index = symptom_index % 2
-            same_game = Game.query.filter(Game.name == sheet.cell(row, 2 * age_index + 1).value, Game.system == system)
-            if (same_game.count() == 0):
-                extra_data = get_giantbomb_data(sheet.cell(row, 2 * age_index + 1), system)
-                game = {}
-                game["system"] = system
-                game["name"] = sheet.cell(row, 2 * age_index + 1).value
-                game["gender"] = sheet.cell(row, 2 * age_index + 2).value
-                game["thumbnail"] = extra_data["thumbnail"]
-                game["image"] = extra_data["image"]
-                game["description"] = extra_data["description"]
-                g = Game(game)
-                db.session.add(g)
+    count = 0
+    current_row = 0
+    print(system)
+    while (count != 12):
+        while (sheet.cell(current_row, 0).value != 1):
+            current_row += 1
+        initial_row = current_row
+        for age_index in range(2):
+            name = sheet.cell(current_row, 2 * age_index + 1).value
+            while (name != ""):
+                print(name)
+                current_row += 1
+                if (current_row == sheet.nrows):
+                    break
+                name = sheet.cell(current_row, 2 * age_index + 1).value
+            if (sheet.ncols < 5):
+                count += 2
+                print("\n")
+                break
+            if (age_index == 0):
+                current_row = initial_row
+            count += 1
+            print("\n")
+            
+
+# NUMBER_RANKING = 25
+# NUMBER_CATEGORIES = 12
+# BEGIN_SPACE = 3
+# CATEGORY_SPACE = 28
+# book = xlrd.open_workbook("Sept2018.xlsx")
+# for sheet in book.sheets():
+#     system = sheet.cell(0,1).value
+#     for symptom_index in range(NUMBER_CATEGORIES):
+#         for row in range(BEGIN_SPACE + CATEGORY_SPACE * math.floor(symptom_index/2), BEGIN_SPACE + CATEGORY_SPACE * math.floor(symptom_index/2) + NUMBER_RANKING, 1):
+#             age_index = symptom_index % 2
+#             same_game = Game.query.filter(Game.name == sheet.cell(row, 2 * age_index + 1).value, Game.system == system)
+#             if (same_game.count() == 0):
+#                 extra_data = get_giantbomb_data(sheet.cell(row, 2 * age_index + 1), system)
+#                 game = {}
+#                 game["system"] = system
+#                 game["name"] = sheet.cell(row, 2 * age_index + 1).value
+#                 game["gender"] = sheet.cell(row, 2 * age_index + 2).value
+#                 game["thumbnail"] = extra_data["thumbnail"]
+#                 game["image"] = extra_data["image"]
+#                 game["description"] = extra_data["description"]
+#                 g = Game(game)
+#                 db.session.add(g)
 
 
 # TEST PRINTING NAMES OF ONE SHEET USING SAME VARIABLES
