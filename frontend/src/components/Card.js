cb0686f35d752fad4992a7593bffe3645fd81e23
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2018-10-24 18:39:43 -0500

    WORK

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Tag from './Tag'
-
+// this is a test
 const cardStyle = {
   color: 'black',
   fontFamily: 'Arial',
