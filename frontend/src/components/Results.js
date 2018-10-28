714310789876b70d024ae4bbac88a62d16b14ca4
Arpan Laha <arpanlaha99@gmail.com>
2018-10-27 20:36:04 -0500

    reformatted

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -9,60 +9,7 @@ const mapStateToProps = state => ({
   results: state.results.games
 })
 
-
 class Results extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-          activeTab: '1',
-          tabGames: ["Xbox", "PS4", "Switch", "That"]
-        };
-      }
-
-      
-    
-      toggle(tab) {
-        if (this.state.activeTab !== tab) {
-          this.setState({
-            activeTab: tab
-          });
-        }
-    }
-    buildCards = (games) => games.map(c => <Card game={c} />)
-    render() {
-        return (
-          <div>
-            <Nav tabs>
-            {Array.from(this.state.tabGames.map(((x,index) => 
-              <NavItem key={index}> 
-                <NavLink
-                  className={classnames({ active: this.state.activeTab === (index + 1).toString() })}
-                  onClick={() => { this.toggle((index + 1).toString()); }}
-                >
-                  {x}
-                </NavLink>
-              </NavItem>
-            )))}
-            </Nav>
-            <TabContent activeTab={this.state.activeTab}>
-              {Array.from(this.state.tabGames.map(((x,index) =>
-              <TabPane tabId= {(index+1).toString()} >
-                <Row>
-                  <Col sm="12">
-                    <h4>Tab {(index+1).toString()} Contents</h4>
-                  </Col>
-                </Row>
-              </TabPane>
-              )))}
-            </TabContent>
-            {this.buildCards(this.props.results)}
-            <br />
-            <Link to={{ pathname: './' }}>Go to home</Link>
-          </div>
-        );
-      }
   constructor(props) {
     super(props)
 
