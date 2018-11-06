56e70d81ce7ca1b098cad1068a48a14e453f2a8f
Angad Garg <angad.garg3@gmail.com>
2018-11-06 03:15:56 -0600

    fixed search by name

--- backend/api/views/search_page.py
+++ backend/api/views/search_page.py
@@ -15,16 +15,21 @@ SEARCH_GAMES_URL = "/search/games"
 @search_page.route(SEARCH_GAMES_URL, methods=["GET"])
 def search_game_by_name():
     data = request.args
-
-    games = []
+    all_empty = True
 
     if data.get("name") is None:
         return create_response(status=400, message="Title of game is required")
     name = data.get("name")
     games = Game.query.filter(func.lower(Game.name).contains(name.lower()))
-    if games.count() == 0:
+    systems = {}
+    for system in Game.system.type.enums:
+        games_by_system = games.filter(Game.system == system).all()
+        if len(games_by_system) != 0:
+            systems[system] = [game.to_dict() for game in games_by_system]
+            all_empty = False
+    if all_empty:
         return create_response(
             status=400, message="No games available with specified name"
         )
 
-    return create_response(status=200, data={"games": serialize_list(games)})
+    return create_response(status=200, data={"games": systems})
--- frontend/src/utils/ApiWrapper.js
+++ frontend/src/utils/ApiWrapper.js
@@ -1,35 +1,46 @@
 import axios from 'axios'
 import BACKEND_URL from './ApiConfig'
 
-function getGames(age, symptom, system) {
+export const getGamesByName = name => {
   return axios
-    .get(BACKEND_URL + '/games' + '?age=' + age + '&symptom=' + symptom + '&system=' + system)
+    .get(BACKEND_URL + '/search/games' + '?name=' + name)
     .then(response => {
       return response.data.result.games
     })
-    .catch(function(error) {
+    .catch(error => {
       console.log('ERROR: ', error)
       return null
     })
 }
 
-function sendFile(file) {
+export const getGames = (age, symptom, system) => {
+  return axios
+    .get(BACKEND_URL + '/games' + '?age=' + age + '&symptom=' + symptom + '&system=' + system)
+    .then(response => {
+      return response.data.result.games
+    })
+    .catch(error => {
+      console.log('ERROR: ', error)
+      return null
+    })
+}
+
+export const sendFile = file => {
   let data = new FormData()
   data.append('file', file)
 
   return axios
-    .post(BACKEND_URL, data)
+    .post(BACKEND_URL + '/games', data)
     .then(response => {
       return {
         type: 'UPLOAD_FILE_SUCCESS',
         response
       }
     })
-    .catch(function(error) {
+    .catch(error => {
       return {
         type: 'UPLOAD_FILE_FAIL',
         error
       }
     })
 }
-export { getGames, sendFile }
