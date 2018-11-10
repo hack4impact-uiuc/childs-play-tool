6374dee1b391cce2fa7ee949be94b928db853d55
Michael Chen <36244017+Feeshay@users.noreply.github.com>
2018-11-10 15:17:31 -0600

    Misc UI stuff (#54)
    * icon ordering
    
    * popup readded
    
    * results spacing
    
    * inline magic?

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Tag from './Tag'
-import styles from '../styles/styles.scss'
+import '../styles/results.scss'
 import Constants from '../utils/Constants'
 
 class Card extends Component {
@@ -13,21 +13,23 @@ class Card extends Component {
   }
   render() {
     return (
-      <p
-        className={this.state.hover}
-        onMouseOver={e => this.setState({ hover: 'cardStyleHover' })}
-        onMouseLeave={e => this.setState({ hover: 'cardStyle' })}
-      >
-        <div className="cardName" align="left">
-          {this.props.game.name}
-        </div>
-        <div align="center">
-          {/*this.props.game.tags ? this.buildTags(this.props.game.tags) : null*/}
-          {/*<Tag type={'age'} tag={this.props.tags[0]} />
+      <div className="resultsBox">
+        <p
+          className={this.state.hover}
+          onMouseOver={e => this.setState({ hover: 'cardStyleHover' })}
+          onMouseLeave={e => this.setState({ hover: 'cardStyle' })}
+        >
+          <div className="cardName" align="left">
+            {this.props.game.name}
+          </div>
+          <div align="center">
+            {/*this.props.game.tags ? this.buildTags(this.props.game.tags) : null*/}
+            {/*<Tag type={'age'} tag={this.props.tags[0]} />
           <Tag type={'symptom'} tag={this.props.tags[1]} />*/}
-        </div>
-        <p>{this.props.game.description ? this.props.game.description : null}</p>
-      </p>
+          </div>
+          <p>{this.props.game.description ? this.props.game.description : null}</p>
+        </p>
+      </div>
     )
   }
 }
