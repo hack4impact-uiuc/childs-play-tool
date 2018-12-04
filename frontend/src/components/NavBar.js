9f573b848c8a4b25ec05b936fdcadb851aab1d06
jeffylin <jlin79@illinois.edu>
2018-12-03 22:20:26 -0600

    npm format complete

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu
+} from 'reactstrap'
+
+import '../styles/landingpage.scss'
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.toggleNavbar = this.toggleNavbar.bind(this)
+    this.state = {
+      collapsed: true
+    }
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+  render() {
+    if (window.innerWidth >= 550) {
+      return (
+        <div>
+          <Navbar color="dark" expand>
+            <NavbarBrand href="/">Home</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav className="bg-dark ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/directorPage">Admin</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#Contacts">Contact Us</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#HowToUse">How to Use</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/search">Search</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins|Source+Sans+Pro"
+            rel="stylesheet"
+          />
+          <Navbar className="navbar-dark bg-dark">
+            <Link to="/">
+              <NavbarBrand>Home</NavbarBrand>
+            </Link>
+            <NavbarToggler onClick={this.toggleNavbar} />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav className="bg-dark mr-auto navbar-dark" navbar>
+                <NavItem>
+                  <NavLink href="/directorPage">Admin</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#Contacts">Contact Us</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#HowToUse">How to Use</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/search">Search</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      )
+    }
+  }
+}
+
+export default NavBar
+
+/*
+	return (
+      <div className="backgroundL">
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins|Source+Sans+Pro"
+          rel="stylesheet"
+        />
+        <ul>
+            <li><a href="/" className="cp" >Home</a></li>
+            <li><a href="/directorPage" className="rest">Admin</a></li>
+            <li><a href="/#Contacts" className="rest">Contact Us</a></li>
+            <li><a href="/#HowToUse" className="rest">How to Use</a></li>
+            <li><a href="/search" className="rest">Search</a></li>
+        </ul>
+      </div>
+    )
+*/
