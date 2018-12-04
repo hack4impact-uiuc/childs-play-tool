092a0989888ced284a22b02365ceb9508fd23808
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2018-12-04 01:08:33 -0600

    santa

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -56,36 +56,40 @@ class Card extends Component {
         <br />
         <br />
         <p className="cardDescription">{this.state.description ? this.state.description : null}</p> */}
-          <div align="center">
-            <div className="cardName">{this.props.game.name}</div>
-            <div style={{height: "40%", width: "40%"}}>
-            <img
-              className="imageCard"
-              src={
-                this.props.game.image == ''
-                  ? require('../styles/placeholderimage.png')
-                  : this.props.game.image
-              }
-            />
-            </div>
-            <br/>
+        <div align="center">
+          <div className="cardName">{this.props.game.name}</div>
+          <br />
+          <img
+            className="imageCard"
+            src={
+              this.props.game.image == ''
+                ? require('../styles/placeholderimage.png')
+                : this.props.game.image
+            }
+          />
+          <br />
+          <br />
+          <div className="tagBox">
             {this.props.game.gender && this.props.game.gender !== 'No Discernable Gender' ? (
-              <Tag type={'gender'} tag={this.props.game.gender} />
+              <Tag type={'gender'} tag={this.props.game.gender} card={true} />
             ) : null}
             {this.props.game.tags ? (
               this.props.game.tags.ages.length === 2 ? (
-                <Tag type={'age'} tag={'All Ages'} />
+                <Tag type={'age'} tag={'All Ages'} card={true} />
               ) : (
-                <Tag type={'age'} tag={this.props.game.tags.ages[0]} />
+                <Tag type={'age'} tag={this.props.game.tags.ages[0]} card={true} />
               )
             ) : null}
             {this.props.game.tags
-              ? this.props.game.tags.symptoms.map(t => <Tag type={'symptom'} tag={t} />)
+              ? this.props.game.tags.symptoms.map(t => <Tag type={'symptom'} tag={t} card={true} />)
               : null}
           </div>
-        <br />
-        <br />
-        <p className="cardDescription">{this.state.description ? this.state.description : null}</p>
+          <br />
+          <br />
+          <p className="cardDescription">
+            {this.state.description ? this.state.description : null}
+          </p>
+        </div>
       </html>
     )
   }
