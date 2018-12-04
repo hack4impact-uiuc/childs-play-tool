f9af9b2fa09c12b57c42255060f6d63e5ba1a5bb
Betty Shen <cs2@illinois.edu>
2018-12-04 00:49:33 -0600

    Editable Cards  (#72)
    * initial commit of editable cards
    
    * formatting
    
    * Add url space for image
    
    * more merge conflicts
    
    * Added API method and request
    
    * finally worksgit status
    
    * more merge conflicts
    
    * fix formatting
    
    * add updateConsole to other searchpage buttons + auth

--- frontend/src/redux/modules/results.js
+++ frontend/src/redux/modules/results.js
@@ -2,9 +2,12 @@
 const SAVE_SEARCH = 'results/SAVE_SEARCH'
 const UPDATE_RESULTS = 'results/UPDATE_RESULTS'
 const GET_SAVED_SEARCH = 'results/GET_SAVED_SEARCH'
+const EDIT_GAME_STATE = 'results/EDIT_GAME_STATE'
+const UPDATE_CONSOLE = 'results/UPDATE_CONSOLE'
 const UPDATE_TAB = 'results/UPDATE_TAB'
 
 export const resultsState = {
+  currentConsole: '',
   searches: [],
   games: {},
   query: {},
@@ -13,6 +16,11 @@ export const resultsState = {
 
 export default function reducer(state = resultsState, action) {
   switch (action.type) {
+    case UPDATE_CONSOLE:
+      return {
+        ...state,
+        currentConsole: action.value
+      }
     case UPDATE_RESULTS:
       return {
         ...state,
@@ -32,7 +40,28 @@ export default function reducer(state = resultsState, action) {
       return {
         ...state,
         games: state.searches.find(({ value }) => value === action.value).searchResults.results,
-        query: state.searches.find(({ value }) => value === action.value).searchResults.query
+        query: state.searches.find(({ value }) => value === action.value).searchResults.query,
+        currentConsole: Object.keys(
+          state.searches.find(({ value }) => value === action.value).searchResults.results
+        )[0]
+      }
+
+    case EDIT_GAME_STATE:
+      return {
+        ...state,
+        games: {
+          ...state.games,
+          [state.currentConsole]: state.games[state.currentConsole].map(
+            item =>
+              item.id === action.payload.gameId
+                ? {
+                    ...item,
+                    description: action.payload.newDescription,
+                    image: action.payload.newImageURL
+                  }
+                : item
+          )
+        }
       }
     case UPDATE_TAB:
       return {
@@ -59,6 +88,21 @@ export const updateResults = value => ({
 
 export const getSavedSearch = value => ({
   type: GET_SAVED_SEARCH,
+  value
+})
+
+export const editGameState = (gameConsole, gameId, newDescription, newImageURL) => ({
+  type: EDIT_GAME_STATE,
+  payload: {
+    gameConsole,
+    gameId,
+    newDescription,
+    newImageURL
+  }
+})
+
+export const updateConsole = value => ({
+  type: UPDATE_CONSOLE,
   value
 })
 
