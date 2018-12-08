cbe62ae2d0be524227e3a1d96531d6014d6a22ac
Hana Rimawi <hanagzr@gmail.com>
2018-12-07 21:37:29 -0600

    Landing page (#109)
    * landing bruh
    
    * more
    
    * lets merge
    
    * lets merge
    
    * historno
    
    * update gitignore
    
    * fixxxes
    
    * fixxxes
    
    * fixxxes
    
    * fixxxes
    
    * fixxxes
    
    * image changing
    
    * more updating
    
    * page so far
    
    * merge master
    
    * make them pics pop
    
    * search again
    
    * format
    
    * Added in search by name onto How to Use
    
    * merged
    
    * format
    
    * butt pads
    
    * teehee
    
    * link
    
    * other landing page idea
    
    * merge
    
    * Landing page is good but Dropdown.js is broken on my branch?
    
    * format
    
    * arrangment
    
    * AY LOOKS GOOD
    
    * AY LOOKS GOOD
    
    * radial

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -16,26 +16,37 @@ class LandingPage extends Component {
 
   render() {
     return (
-      <div className="backgroundL" style={{ paddingTop: window.innerWidth >= 550 ? '10%' : '20%' }}>
-        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" />
-        <h3 className="homeTextL">{LandingPageStrings['greeting']}</h3>
-        <h3 className="subtitles">{LandingPageStrings['subtitle']}</h3>
-        <p className="padded">{LandingPageStrings['purpose']}</p>
-        <Link to="/search">
-          <Button className="buttonpad">{LandingPageStrings['searchButton']}</Button>
-        </Link>
-        <div>
-          <a href="http://childsplaycharity.org/assets/downloads/booklet.pdf">
-            {' '}
-            {LandingPageStrings['englishPDF']}
-          </a>
-          <br />
-          <a href="http://childsplaycharity.org/assets/downloads/booklet.pdf">
-            {' '}
-            {LandingPageStrings['spanishPDF']}
-          </a>
+      <div className="backgroundL">
+        <div className="rowcolor2">
+          <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" />
+          <img
+            className="cleared2"
+            src={require('../styles/cplogo.png')}
+            alt="Child's Play logo"
+            height="220px"
+          />
+          <h3 className="homeTextL">Welcome!</h3>
+          <h3 className="subtitles2">Therapeutic&nbsp; Video&nbsp; Game&nbsp; Guide</h3>
+          <p className="padded">
+            This guide was designed as a quick reference to help caretakers quickly select games for
+            their patients. Caretakers can reference the category that best fits the symptoms of the
+            patient and select one of the games listed. The games recommended in this guide were
+            curated by researchers at EEDAR, a market-leading video game research firm in
+            collaboration with mental health researchers at UCSD.
+          </p>
+          <Link to="/search">
+            <Button className="buttonpad">{LandingPageStrings['searchButton']}</Button>
+          </Link>
+          <div>
+            <a href="https://childsplaycharity.org/assets/downloads/booklet.pdf">
+              {' '}
+              {LandingPageStrings['englishPDF']}
+            </a>
+            <br />
+            <a href="https://childsplaycharity.org/tvgg-es"> {LandingPageStrings['spanishPDF']}</a>
+          </div>
+          <div className="line" />
         </div>
-        <div className="line" />
 
         <Container id="HowToUse" className="separator">
           <h3 className="homeText2L">{LandingPageStrings['tutorialHeader']}</h3>
@@ -45,15 +56,7 @@ class LandingPage extends Component {
               <img
                 src={require('../styles/searchbyname.png')}
                 alt="Search by name field"
-                height="100px"
-              />
-            </Col>
-            <Col className="mspace">
-              <p>{LandingPageStrings['tutorial1']}</p>
-              <img
-                src={require('../styles/copysearchurl.png')}
-                alt="Copy search URL field"
-                height="100px"
+                height="80px"
               />
             </Col>
           </Row>
@@ -86,74 +89,124 @@ class LandingPage extends Component {
             </Col>
           </Row>
           <Row>
-            <Col>
+            <Col className="mspace">
               <p>{LandingPageStrings['tutorial6']}</p>
               <img src={require('../styles/inputsave.png')} alt="Search page" height="140px" />
             </Col>
-            <Col>
+            <Col className="mspace">
               <p>{LandingPageStrings['tutorial7']}</p>
               <img src={require('../styles/loadsave.png')} alt="Search page" height="110px" />
             </Col>
           </Row>
-        </Container>
-        <div className="line" />
-
-        <Container className="separator">
-          <h3 className="homeText2L">{LandingPageStrings['mobileGuideHeader']}</h3>
           <Row>
             <Col>
-              <h2 className="subtitles">{LandingPageStrings['android']}</h2>
-              <p>{LandingPageStrings['androidDesc']}</p>
-            </Col>
-            <Col>
-              <h2 className="subtitles">{LandingPageStrings['iOS']}</h2>
-              <p>{LandingPageStrings['iOSDesc']}</p>
+              <p>{LandingPageStrings['tutorial1']}</p>
+              <img
+                src={require('../styles/copysearchurl.png')}
+                alt="Copy search URL field"
+                height="80px"
+              />
             </Col>
           </Row>
         </Container>
         <div className="line" />
 
-        <Container id="Contacts" className="separator">
+        <div className="rowcolor2">
+          <Container className="separator">
+            <h3 className="homeText2L">{LandingPageStrings['mobileGuideHeader']}</h3>
+            <Row>
+              <Col>
+                <h2 className="subtitles">{LandingPageStrings['android']}</h2>
+                <p>{LandingPageStrings['androidDesc']}</p>
+              </Col>
+              <Col>
+                <h2 className="subtitles">{LandingPageStrings['iOS']}</h2>
+                <p>{LandingPageStrings['iOSDesc']}</p>
+              </Col>
+            </Row>
+          </Container>
+          <div className="line" />
+        </div>
+
+        <Container id="Contacts" className="separator2">
           <Row>
             <Col>
-              <h3> {LandingPageStrings['aboutHeader']}</h3>
+              <h3> About Child&#39;s Play</h3>
               <img
                 className="cleared"
                 src={require('../styles/cp-logo.png')}
                 alt="Child's Play logo"
                 height="100px"
               />
-              <p>{LandingPageStrings['aboutDesc']}</p>
-              <a href="http://childsplaycharity.org"> {LandingPageStrings['learnMore']} </a>
-            </Col>
-            <Col>
-              <h3>{LandingPageStrings['contactHeader']}</h3>
-              <p className="address">
-                <br />
-                {LandingPageStrings['contactCompany']} <br />
-                {LandingPageStrings['contactAddress']}
-                <br />
-                {LandingPageStrings['contactAddress2']}
-                <br />
-                {LandingPageStrings['contactAddress3']}
-                <br />
-                <br />
-                {LandingPageStrings['facebook']}{' '}
-                <a href="https://www.facebook.com/ChildsPlayCharity/">
-                  {LandingPageStrings['facebookName']}
-                </a>
-                <br />
-                {LandingPageStrings['twitter']}{' '}
-                <a href="https://twitter.com/CPCharity/">{LandingPageStrings['twitterHandle']}</a>{' '}
-                <br />
-                <a href="https://childsplaycharity.org/about#contact">
-                  {' '}
-                  {LandingPageStrings['contactMore']}{' '}
-                </a>
+              <p>
+                Child&#39;s Play is a game industry charity dedicated to improving the lives of
+                children with toys and games in a network of over 150 hospitals worldwide. With the
+                help of hospital staff, gift wish lists are set up full of video games, toys, books,
+                and other fun stuff for kids. These can provide vital distraction from an otherwise
+                generally unpleasant experience.
               </p>
+              <a href="https://childsplaycharity.org"> Learn More </a>
             </Col>
           </Row>
         </Container>
+
+        <div className="contacts">
+          <Container>
+            <Row>
+              <Col xs="12" sm="6">
+                <h3 ref={this.contactRef}>Contact Information </h3>
+                <p className="address">
+                  Address:
+                  <br />
+                  Child’s Play <br />
+                  9660 153rd Ave NE
+                  <br />
+                  Redmond, WA 98052
+                </p>
+              </Col>
+
+              <Col xs="12" sm="6">
+                <p>
+                  For corporate sponsorships, student or media interviews, or partnership inquiries
+                  contact{' '}
+                  <a href="childsplaycharity@childsplaycharity.org">
+                    childsplaycharity@childsplaycharity.org
+                  </a>
+                  . For hospital additions, wishlist updates, or beneficiary inquiries contact{' '}
+                  <a href="foundations@childsplaycharity.org">foundations@childsplaycharity.org</a>.
+                </p>
+                <p>
+                  Facebook:{' '}
+                  <a href="https://www.facebook.com/ChildsPlayCharity/">ChildsPlayCharity</a>
+                  <br />
+                  Twitter: <a href="https://twitter.com/CPCharity/">@CPCharity</a> <br />
+                  <a href="https://childsplaycharity.org/about#contact">
+                    {' '}
+                    More Contact Information{' '}
+                  </a>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="footer">
+          <Container>
+            <Row>
+              <img
+                className="cleared3"
+                src={require('../styles/applogo_white.png')}
+                alt="Hack4impact logo"
+                height="35px"
+              />
+              <p className="spaceme">
+                <a href="https://uiuc.hack4impact.org/">https://uiuc.hack4impact.org/</a> <br />
+                <a href="https://github.com/hack4impact-uiuc/childs-play-tool">
+                  https://github.com/hack4impact-uiuc/childs-play-tool
+                </a>
+              </p>
+            </Row>
+          </Container>
+        </div>
       </div>
     )
   }
